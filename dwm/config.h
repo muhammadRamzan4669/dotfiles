8bb6cad65a4152be715e5fd1a6dafc3146244f4f
Muhammad Haris <ramzanlynx@outlook.com>
2026-04-20 01:28:29 +0500

    remove border around windows

--- dwm/config.h
+++ dwm/config.h
@@ -9,7 +9,7 @@ static const char *up_bright[]   = { "brightnessctl", "set", "+5%", NULL };
 static const char *down_bright[] = { "brightnessctl", "set", "5%-", NULL };
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
